d612e81a1180dea8812be28e9fe22a3f6937036e
santmailo <iamsunil006@gmail.com>
2023-07-10 23:04:46 +0530

    submitting code-Mon Jul 10 2023 23:04:44 GMT+0530 (India Standard Time)

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+//your code here
+let addTodo = document.getElementById("addTodoBtn");
+let input = document.getElementById("newTodoInput");
+let errorMsg = document.getElementById("error")
+let ol = document.getElementById("todoList");
+addTodo.addEventListener("click", addList);
+
+function addList(){
+    let val = input.value;
+    console.log(typeof val);
+    
+    if(val === ""){   
+        errorMsg.style.display = "block";
+        return;
+    }
+    else{
+        errorMsg.style.display = "none";
+        let li = document.createElement("li");
+        li.innerText = val;
+        ol.appendChild(li);
+        input.value = "";
+    }
+}
+
 
